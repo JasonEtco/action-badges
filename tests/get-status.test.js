0b6cfbe7ded43a616d229cffe25f84208d584f70
John Letey <john.letey@colorado.edu>
2019-04-04 11:04:22 +0100

    Add `-/- ___` reporting

--- tests/get-status.test.js
+++ tests/get-status.test.js
@@ -3,17 +3,17 @@ const getStatus = require('../lib/get-status')
 describe('getStatus', () => {
   it('returns `passing` if all check suites have `conclusion: success`', () => {
     const actual = getStatus([{ conclusion: 'success' }])
-    expect(actual).toBe('passing')
+    expect(actual).toBe('1/1 passing')
   })
 
   it('returns `failing` if any check suites have a failing conclusion', () => {
     const actual = getStatus([{ conclusion: 'timed_out' }])
-    expect(actual).toBe('failing')
+    expect(actual).toBe('1/1 failing')
   })
 
   it('returns `neutral` if all check suites have `conclusion: neutral`', () => {
     const actual = getStatus([{ conclusion: 'neutral' }])
-    expect(actual).toBe('neutral')
+    expect(actual).toBe('1/1 neutral')
   })
 
   it('returns `mixed` if it gets confused', () => {
